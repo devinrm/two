eb13f077ed2e9a5ea86e2a666ca24666e04c68ff
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-06-29 00:03:53 -0400

    Add user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :developer_level
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "requires a name" do
+    user = User.new(name: "")
+
+    user.valid?
+
+    expect(user.errors[:name].any?).to eq(true)
+  end
+
+  it "requires a developer_level" do
+    user = User.new(developer_level: "")
+
+    user.valid?
+
+    expect(user.errors[:developer_level].any?).to eq(true)
+  end
 end
