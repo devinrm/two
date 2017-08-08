20889a6cf124f2c3f394d10534d0e18a8f2e701b
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-08-08 00:05:21 -0400

    Making the pairing spec less ridiculous and more closely...
    reflect reality.

--- spec/features/pairing_spec.rb
+++ spec/features/pairing_spec.rb
@@ -9,7 +9,7 @@ xdescribe 'Pairing Users', js: true do
     click_button 'Create User'
 
     within('table.users') do
-      expect(page).to have_content('Hey Devin')
+      expect(page).to have_content('Hey Eliza')
       expect(page).to have_content('Senior Developer')
     end
   end
