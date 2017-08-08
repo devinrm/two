8300b1b6acf902d268ce097802ede91885df5c1f
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-08-08 00:25:43 -0400

    Fix typo

--- app/javascript/test/testHelper.js
+++ app/javascript/test/testHelper.js
@@ -9,17 +9,17 @@ chai.use(chaiEnzyme())
 // http://airbnb.io/enzyme/docs/guides/jsdom.html
 const { JSDOM } = require('jsdom')
 const jsdom = new JSDOM('<!doctype html><html><body></body></html')
-const { window } = jsdom 
+const { window } = jsdom
 const copyProps = (src, target) => {
   const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'underfined')
+    .filter(prop => typeof target[prop] === 'undefined')
     .map(prop => Object.getOwnPropertyDescriptor(src,prop))
   Object.defineProperties(target, props)
 }
 
 global.window = window
 global.document = window.document
-global.fetch = fetch 
+global.fetch = fetch
 global.navigator = { userAgent: 'node.js' }
 
 copyProps(window, global)
