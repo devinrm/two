8053fe33e2c8882a3f864d86fe4ed9feec40e871
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-08-27 19:45:28 -0400

    Style the generated list of pairs

--- app/javascript/components/PairsList.jsx
+++ app/javascript/components/PairsList.jsx
@@ -4,12 +4,12 @@ export default class PairsList extends React.Component {
   constructor(props) {
     super(props)
   }
-  
+
   renderPairs = () => {
     return (
-      this.props.pairs.map((pair, index) => 
+      this.props.pairs.map((pair, index) =>
         <li key={index}>
-          {pair.pair_one} | {pair.pair_two}
+          {pair.pair_one} & {pair.pair_two}
         </li>
       )
     )
